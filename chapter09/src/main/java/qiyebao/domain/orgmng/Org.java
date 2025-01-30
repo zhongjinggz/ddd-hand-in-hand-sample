84cac6b62489f99648a314f9cedd425b8bfa81c0
Zhong Jing <zhongjinggz@163.com>
2025-01-30 20:27:34 +0800

    双向导航

--- chapter09/src/main/java/qiyebao/domain/orgmng/Org.java
+++ chapter09/src/main/java/qiyebao/domain/orgmng/Org.java
@@ -1,4 +1,40 @@
 package qiyebao.domain.orgmng;
 
 public class Org {
+    private Long id;
+    private OrgType orgType;         // [1]
+    private String name;
+    private String status;
+
+    public OrgType getOrgType() {
+        return orgType;
+    }
+
+    public void setOrgType(OrgType orgType) {
+        this.orgType = orgType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- chapter09/src/main/java/qiyebao/domain/orgmng/OrgType.java
+++ chapter09/src/main/java/qiyebao/domain/orgmng/OrgType.java
@@ -1,4 +1,42 @@
 package qiyebao.domain.orgmng;
 
+import java.util.Set;
+
 public class OrgType {
+    private String code;
+    private Set<Org> orgs;
+    private String name;
+    private String status;
+
+    public Set<Org> getOrgs() {
+        return orgs;
+    }
+
+    public void setOrgs(Set<Org> orgs) {
+        this.orgs = orgs;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
