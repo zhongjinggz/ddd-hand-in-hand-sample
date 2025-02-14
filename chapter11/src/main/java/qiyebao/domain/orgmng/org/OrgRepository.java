ee27a2badced97c4bcc46c7a98ba2b45cc8783c6
Zhong Jing <zhongjinggz@163.com>
2025-02-14 18:02:50 +0800

    修改OrgRepository中方法的顺序，按增删改查排序。

--- chapter11/src/main/java/qiyebao/domain/orgmng/org/OrgRepository.java
+++ chapter11/src/main/java/qiyebao/domain/orgmng/org/OrgRepository.java
@@ -3,13 +3,14 @@ package qiyebao.domain.orgmng.org;  // [1] 注意，仓库接口在领域层
 import java.util.Optional;
 
 public interface OrgRepository {
+    Org add(Org org);
+
+    int modify(Org org);
+
     Optional<Org> findByIdAndStatus(Long tenantId, Long id, OrgStatus status);
 
     Optional<Org> findById(long tenantId, long id);
 
-    Org add(Org org);
-
     boolean existsBySuperiorAndName(Long tenantId, Long superiorId, String name);
 
-    int modify(Org org);
 }
