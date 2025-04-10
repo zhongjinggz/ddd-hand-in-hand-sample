e1bf3b408758b6ab847648af81bfe4257bec2230
Zhong Jing <zhongjinggz@163.com>
2025-04-10 16:10:20 +0800

    complete SkillType and SkillTypeRepository

---
+++ chapter14/src/main/java/qiyebao/adapter/driven/persistence/orgmng/SkillTypeRepositoryJdbc.java
@@ -0,0 +1,32 @@
+package qiyebao.adapter.driven.persistence.orgmng;
+
+import org.springframework.stereotype.Repository;
+import qiyebao.common.framework.adapter.driven.persistence.JdbcHelper;
+import qiyebao.domain.orgmng.posttype.PostType;
+import qiyebao.domain.orgmng.posttype.PostTypeRepository;
+import qiyebao.domain.orgmng.skilltype.SkillType;
+import qiyebao.domain.orgmng.skilltype.SkillTypeRepository;
+
+@Repository
+public class SkillTypeRepositoryJdbc implements SkillTypeRepository {
+    private final JdbcHelper jdbc;
+
+    public SkillTypeRepositoryJdbc(JdbcHelper jdbc) {
+        this.jdbc = jdbc;
+     }
+
+    @Override
+    public boolean existsByIdAndStatus(Long tenantId
+        , Long id
+        , SkillType.Status status
+    ) {
+        final String sql = "select 1"
+                + " from skill_type "
+                + " where tenant_id = ? "
+                + " and id = ?"
+                + " and status_code = ?"
+                + " limit 1";
+
+        return jdbc.selectExists(sql, tenantId, id, status.getCode());
+    }
+}
---
+++ chapter14/src/main/java/qiyebao/domain/orgmng/skilltype/SkillTypeRepository.java
@@ -0,0 +1,7 @@
+package qiyebao.domain.orgmng.skilltype;
+
+import qiyebao.domain.orgmng.posttype.PostType;
+
+public interface SkillTypeRepository {
+    boolean existsByIdAndStatus(Long tenantId, Long id, SkillType.Status status);
+}
